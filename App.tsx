5c0ae9dc4f620d953699db4cd974e4fa19ebe70a
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-01-22 09:00:15 +0100

    Signals not appeared on homescreen first load

--- App.tsx
+++ App.tsx
@@ -13,11 +13,23 @@ import mergeOptions from 'merge-options';
 
 const Drawer = createDrawerNavigator();
 
+const homeScreenListeners = ({ navigation, route }) => ({
+  focus: async (e) => {
+    console.log('HomeScreen focused.');
+    await store.init();
+    navigation.setParams({
+      signals: store.getSignalsArray(),
+    });
+  }
+})
+
 const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="HomeScreen">
         <Drawer.Screen name="HomeScreen" component={HomeScreen}
+          initialParams={{ signals: [] }}
+          listeners={homeScreenListeners}
           options={{ drawerLabel: "Signals", title: "Signals" }}
         />
         <Drawer.Screen name="Details" component={DetailsScreen}
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -5,14 +5,10 @@ import { styles } from '../Styles';
 import { store } from '../data/SignalStorage';
 
 export default function HomeScreen({ navigation, route }) {
-    useEffect(() => {
-        const unsubscribe = navigation.addListener('focus', async () => {
-            await store.init();
-        });
-        return unsubscribe;
-    }, [navigation]);
 
-    if (store.getSignalsArray().length == 0) {
+    let signals = route.params.signals;
+
+    if (signals.length == 0) {
         return (
             <View style={{ height: '100%', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
                 <Text>List is empty</Text>
@@ -23,7 +19,7 @@ export default function HomeScreen({ navigation, route }) {
             <ScrollView>
                 <View style={styles.scrollContainer}>
                     {
-                        store.getSignalsArray().map((signal) => {
+                        signals.map((signal) => {
                             return (
                                 <View key={signal.key} style={styles.buttonContainer}>
                                     <MyButton
